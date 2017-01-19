37efdd9eebce14e99d33fc50808a9e1792a7b252
Chennamouli <mouli.3022@gmail.com>
2017-01-19 11:17:50 -0600

    Added bootstrap 4 and ngb

--- src/main/resources/angularjs/src/app/app.module.ts
+++ src/main/resources/angularjs/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { NavComponent } from './component/nav.component';
 import {PageNotFoundComponent} from './component/page-not-found.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent, HomeComponent, DashboardComponent,NavComponent,PageNotFoundComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule, AppRoutingModule
