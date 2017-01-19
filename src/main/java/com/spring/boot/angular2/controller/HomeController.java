01cf1d5b1a57c00d93436147d7150f5ac0af6a18
Chennamouli <mouli.3022@gmail.com>
2017-01-19 14:54:46 -0600

    updated logout link

--- src/main/java/com/spring/boot/angular2/controller/HomeController.java
+++ src/main/java/com/spring/boot/angular2/controller/HomeController.java
@@ -1,16 +1,23 @@
 package com.spring.boot.angular2.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
-public class HomeController extends BaseController{
+public class HomeController extends BaseController {
 
-@GetMapping
+	@GetMapping
 	public ModelAndView home(Model model) {
 		return new ModelAndView("index");
 	}
@@ -18,6 +25,20 @@ public class HomeController extends BaseController{
 	@RequestMapping(value = { "/home" })
 	public ModelAndView home2(Model model) {
 		return new ModelAndView("index");
+	}
+
+	/*
+	 * http://websystique.com/spring-security/spring-security-4-logout-example/
+	 */
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login?logout";// You can redirect wherever you want,
+										// but generally it's a good practice to
+										// show login screen again.
 	}
 
 }
